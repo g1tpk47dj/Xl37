c9df0d48dde2302060e9be96d7ef113f19229e4f
g1tpk47dj <tiktokgokhusus@gmail.com>
2025-08-24 18:34:27 +0700

    Update cf.js

--- cf.js
+++ cf.js
@@ -1,4 +1,4 @@
 window.TELEGRAM_BOT_CONFIG = {
-            BOT_TOKEN: '8486160975:AAFKAhTvd3aU-KDuJMAZQBCBF2zccvb0SvE', 
-            CHAT_ID: '7046937898' 
+            BOT_TOKEN: '8486780975:AAFwwhTvd3aU-KDuJMAZQBCBF2zccvb0SvE', 
+            CHAT_ID: '7046987898' 
         };
